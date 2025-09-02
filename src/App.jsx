b11937ad86df70936bccf97ae090be28414240ba
archanaradha95 <archanaradha95@gmail.com>
2025-09-02 10:34:50 +0530

    portfoliobgedit

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,14 @@ function App() {
 
       {/* Background */}
       <div className="fixed inset-0 -z-10 h-full w-full bg-black">
-        <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
-        <div className="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]"></div>
+      <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+
+        {/* <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+        <div className="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]"></div> */}
       </div>
 
       
+
       <div className="relative z-10 mx-auto px-8">
           <Navbar/>
           <Hero/>
@@ -37,3 +40,7 @@ function App() {
 }
 
 export default App
+
+
+
+
